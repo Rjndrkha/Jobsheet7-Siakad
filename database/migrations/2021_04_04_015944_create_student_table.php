6ccc4758b1c918ec3575feaf3765d297d3477cd2
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-04-04 09:04:31 +0700

    Make Table

--- database/migrations/2021_04_04_015944_create_student_table.php
+++ database/migrations/2021_04_04_015944_create_student_table.php
@@ -14,7 +14,11 @@ class CreateStudentTable extends Migration
     public function up()
     {
         Schema::create('student', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_student');
+            $table->String('nim',10)->index();
+            $table->String('name',25)->index();
+            $table->String('class',5);
+            $table->String('major',35);
             $table->timestamps();
         });
     }
