afd422ccd40a6f2f040c72c511a7978a574aabae
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-04-04 09:27:57 +0700

    ADD

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Student extends Model
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
+class Student extends Model // Model definition
 {
-    use HasFactory;
-}
+    protected $table = 'student'; // Eloquent will create a student model to store records in the student table
+    protected $primaryKey = 'nim'; // Calling DB contents with primary key
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Nim',
+        'Name',
+        'Class',
+        'Major',
+    ];
+};
